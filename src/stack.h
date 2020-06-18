fa06fe68f016b66066207106af3d05d47a196e58
Henrik Christensen <sensen1695@hotmail.com>
2020-06-18 12:07:40 +0200

    added license

--- src/stack.h
+++ src/stack.h
@@ -1,10 +1,28 @@
+/*
+ * Stack header file
+ * Copyright (C) 2020 Henrik Abel Christensen
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *		
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *						
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef STACK_H
 #define STACK_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "alloc.h"
+#include "utils.h"
 #include "node.h"
 
 #define EMPTY_STACK 0
